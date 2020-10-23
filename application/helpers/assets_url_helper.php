053aac7ef5692df41437144031046886b6e7348f
Sisca Ayu Febrianti <siscayufebrianti@gmail.com>
2020-10-23 14:08:58 +0700

    helper

--- application/helpers/assets_url_helper.php
+++ application/helpers/assets_url_helper.php
@@ -1,8 +1,8 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) $this->getResponse()->setBody('No direct script access allowed');
 
-if ( ! function_exists('asset_url()')) {
-    function asset_url()
-   {
-    return base_url().'assets/';
-   }
- }
+if (!function_exists('asset_url()')) {
+  function asset_url()
+  {
+    return base_url() . 'assets/';
+  }
+}
